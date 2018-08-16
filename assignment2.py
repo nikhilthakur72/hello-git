41f873836f941ec0fac200c25787787e4fcba44b
nikhikthakur72 <nikhilthakur031@gmail.com>
2018-08-17 00:57:11 +0530

    new

---
+++ assignment2.py
@@ -0,0 +1,33 @@
+#1>
+print("python language")
+
+
+#2>
+str1 = "language"
+str2 = "python"
+print(str1+str2)
+
+
+#3>
+a = int(input("enter the number "))
+b = int(input("enter the number"))
+c = int(input("enter the number"))
+print("a = ",a,"b = ",b,"c = ",c)
+
+
+#4>
+print("let's get started")
+
+#5>
+s = "Acadview"
+course = "python"
+fees = 5000
+str1 = "{} {} course fee is {}".format(s,course,fees)
+print(str1)
+
+#6>
+name="Tony Stark"
+salary=1000000
+print("%s,%d"%(name,salary))
+
+
